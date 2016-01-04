268366ddeef5d79c11a2bb92834d3bbffb47cf94
Steffen Pegenau <steffen.pegenau@hrz.tu-darmstadt.de>
2016-01-04 12:21:16 +0100

    Initial commit

---
+++ app/scripts/app.js
@@ -0,0 +1,15 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name supporterApp
+ * @description
+ * # supporterApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('supporterApp', [
+    'ngAnimate',
+    'ngTouch'
+  ]);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name supporterApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the supporterApp
+ */
+angular.module('supporterApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
