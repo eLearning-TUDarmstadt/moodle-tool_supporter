416812977bc150825779c32ff28db94c157e5aaa
KlaraSaary <klara.saary@gmail.com>
2017-08-15 14:30:00 +0200

    Adding new function to convert user and course table to datatable
    Now user_table and course_table are rendered via DataTable() and not via php

--- amd/src/datatables.js
+++ amd/src/datatables.js
@@ -23,46 +23,92 @@
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  * @since      3.1.1
  */
-define(['jquery', 'tool_supporter/jquery.dataTables', 'core/str', 'tool_supporter/table_filter'], function($, datatables, str, filter) {
+define(['jquery', 'tool_supporter/jquery.dataTables', 'core/str', 'tool_supporter/table_filter', 'core/ajax', 'core/notification', 'core/templates'], 
+        function($, datatables, str, filter, ajax, notification, templates) {
 
-  return /** @alias module:tool_supporter/datatables */ {
+  
+    return /** @alias module:tool_supporter/datatables */ {
 
       /**
        *
        * @method use_dataTable
+       * @param tableID : ID of table you want to convert into datatable
+       * @param filterSelector : Arrays with information for the function filterEvent in table_filter.js. Every array has three parameters: 
+       * There can be several filterSelectors, for example one for each dropdown-menue 
        */
-
-       //There can be several filterSelectors, for example one for each dropdown-menue
        use_dataTable: function(tableID, filterSelector){
         var args = arguments;
         str.get_string('search', 'moodle').done(function(searchString) {
           $(tableID).DataTable({
             "retrieve": true, //So the table can be accessed after initialization
             "responsive": true,
-            "lengthChange": false,
+            "lengthChange": true,
             "pageLength": 30,
             "language": {
               //Empty info. Legacy: Showing page _PAGE_ of _PAGES_
               'info': "",
-              //'search': searchString+": "
+              'search': searchString+": ",
+              'lengthMenu': "_MENU_"
             },
-            "dom": "<'w-100'<'col'f>>" +
+            /*"dom": "<'w-100'<'col'f>>" +
               "<'w-100'<'col't>>" +
-              "<'w-100'<'col-sm-6'i><'col-sm-6'p>>",
+              "<'w-100'<'col-sm-6'i><'col-sm-6'p>>",*/
+            "dom": '<lf>t<ip>',
             "paging": true,
             "pagingType": "numbers",
-            "scrollX": true
+            "scrollX": "true"
           });
 
           var i;
           for(i=1; i < args.length; i++){
             if(args[i]){
-            filter.filterEvent(args[i][0], args[i][1], args[i][2], tableID)
+            filter.filterEvent(args[i][0], args[i][1], args[i][2], tableID);
             }
           };
-          $('[data-region="course_table"]').show();
-          $('[data-region="user_table"]').show();
         });
-    },
+       },
+    
+          /**
+       *
+       * @method dataTable_ajax
+       * @param tableID : ID of table you want to convert into datatable
+       */
+    dataTable_ajax: function(tableID, methodname, args, datainfo, columns){
+        
+         var promise = ajax.call([{
+                "methodname": methodname,
+                "args": args
+            }]);
+   
+        var otable;
+
+        promise[0].done(function(data) {
+          
+          str.get_string('search', 'moodle').done(function(searchString) {
+             otable = $(tableID).DataTable( {
+                 "data": data[datainfo],
+                 "columns":columns, 
+                 "retrieve": true, //So the table can be accessed after initialization
+                 "responsive": true,
+                 "lengthChange": true,
+                 "pageLength": 30,
+                 "language": {
+                     //Empty info. Legacy: Showing page _PAGE_ of _PAGES_
+                      'info': " ",
+                      'search': searchString+": ",
+                      'lengthMenu': "_MENU_"
+                     },
+                  /*"dom": "<'w-100'<'col'f>>" +
+                       "<'w-100'<'col't>>" +
+                       "<'w-100'<'col-sm-5'i><'col-sm-6'p><'col-sm-1'l>>",*/
+                "dom": '<lf>t<ip>',
+                 "paging": true,
+                 "pagingType": "numbers",
+                 "lengthMenu": [ 10, 25, 50, 75, 100 ],
+                 "scrollX": true
+             });
+         });
+      }).fail(notification.exception);
+    }
   };
 });
