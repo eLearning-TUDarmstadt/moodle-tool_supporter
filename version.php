34855e33ced98c044543822841dd2f6ff5fb9830
my-curiosity <my_cur10s1ty@protonmail.com>
2023-05-09 10:58:45 +0200

    Change required moodle version to 4.0

--- version.php
+++ version.php
@@ -22,7 +22,7 @@
  */
 defined('MOODLE_INTERNAL') || die();
 $plugin->version   = 2021101105; // The current plugin version (Date: YYYYMMDDXX).
-$plugin->requires  = 2020110900; // Requires Moodle version 3.10 or higher.
+$plugin->requires  = 2022041900; // Requires Moodle version 3.10 or higher.
 $plugin->component = 'tool_supporter'; // Full name of the plugin (used for diagnostics).
 $plugin->release = 'v1.02';
 $plugin->maturity = MATURITY_STABLE;
