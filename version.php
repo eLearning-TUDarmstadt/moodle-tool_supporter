097a78e9c1b245015dedc51897484bc3c29f73c8
my-curiosity <my_cur10s1ty@protonmail.com>
2023-05-23 16:15:50 +0200

    Filtering fix from dev

--- version.php
+++ version.php
@@ -21,7 +21,7 @@
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 defined('MOODLE_INTERNAL') || die();
-$plugin->version   = 2023052301; // The current plugin version (Date: YYYYMMDDXX).
+$plugin->version   = 2023052302; // The current plugin version (Date: YYYYMMDDXX).
 $plugin->requires  = 2020110900; // Requires Moodle version 3.10 or higher.
 $plugin->component = 'tool_supporter'; // Full name of the plugin (used for diagnostics).
 $plugin->release = 'v1.05';
